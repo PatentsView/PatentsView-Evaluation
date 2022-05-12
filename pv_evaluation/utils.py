57dc199866152c67a23b8500f15b1ec05e565e48
Olivier Binette <olivier.binette@gmail.com>
2022-05-12 08:47:26 -0400

    Update yaxis range on combined plots

--- pv_evaluation/utils.py
+++ pv_evaluation/utils.py
@@ -12,5 +12,6 @@ def compare_plots(*figs):
         combined.add_traces(fig.data)
 
         combined.update_xaxes(range=fig.layout.xaxis.range)
+        combined.update_yaxes(range=fig.layout.yaxes.rage)
 
     return combined
