606b4d4e486e466faf79f24e11c21c6223f60512
Olivier Binette <olivier.binette@gmail.com>
2022-05-16 10:48:19 -0400

    update

--- pv_evaluation/metrics/pairwise.py
+++ pv_evaluation/metrics/pairwise.py
@@ -11,23 +11,23 @@ def links_count(membership_vect):
     return np.sum(comb(cluster_sizes(membership_vect), 2))
 
 
-def true_positives(prediction: pd.Series, reference: pd.Series):
+def true_positives_count(prediction: pd.Series, reference: pd.Series):
     I = np.intersect1d(prediction.index, reference.index)
     TP_cluster_sizes = np.unique((prediction[I], reference[I]), axis=1, return_counts=True)[1]
 
     return np.sum(comb(TP_cluster_sizes, 2))
 
 
-def false_positives(prediction, reference):
-    return links_count(prediction) - true_positives(prediction, reference)
+def false_positives_count(prediction, reference):
+    return links_count(prediction) - true_positives_count(prediction, reference)
 
 
 def raw_precision(prediction, reference):
-    return true_positives(prediction, reference) / links_count(prediction)
+    return true_positives_count(prediction, reference) / links_count(prediction)
 
 
-def raw_recall(pred_labels, true_labels):
-    return raw_precision(true_labels, pred_labels)
+def raw_recall(prediction, reference):
+    return raw_precision(reference, prediction)
 
 
 def raw_precision_recall(pred_labels, true_labels):
