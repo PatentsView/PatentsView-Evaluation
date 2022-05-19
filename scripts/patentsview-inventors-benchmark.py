e68d43ebf5fe9bc7f11a3cf14ad1ff0f1ead5276
Olivier Binette <olivier.binette@gmail.com>
2022-05-19 16:45:37 -0400

    resolve conflict

--- scripts/patentsview-inventors-benchmark.py
+++ scripts/patentsview-inventors-benchmark.py
@@ -5,13 +5,9 @@ except:
 
 data = pd.read_csv("data-raw/patentsview-inventor/gold-labels.txt", sep="\t", header=None)
 
-<<<<<<< HEAD
-data["mention-id"] = "US" + data[0]
-=======
 mention_numbers = data[0].str.split("-").values
 
 data["mention-id"] = list(map(lambda x: "US"+x[0].lstrip('0')+'-'+x[1], mention_numbers))
->>>>>>> benchmark
 data["unique-id"] = data[1]
 
 data[["mention-id", "unique-id"]].to_csv("pv_evaluation/data/inventor/patentsview-inventors-benchmark.csv", index=False)
