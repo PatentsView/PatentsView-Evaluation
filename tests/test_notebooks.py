b3772624d30ba0dcf69f8d6822bc2b76a94d328b
Ubuntu <olivier.binette@gmail.com>
2022-06-17 14:27:22 +0000

    refactor tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,17 +5,21 @@ from testbook import testbook
 def test_executable_notebook(tb):
     pass
 
+
 @testbook("tests/notebooks/metrics/test_pairwise.ipynb", execute=True)
 def test_executable_notebook(tb):
     pass
+
 
 @testbook("tests/notebooks/metrics/test_cluster.ipynb", execute=True)
 def test_executable_notebook(tb):
     pass
 
+
 @testbook("tests/notebooks/benchmark/benchmark-datasets.ipynb", execute=True)
 def test_executable_notebook(tb):
     pass
+
 
 @testbook("tests/notebooks/summary/inventor.ipynb", execute=True)
 def test_executable_notebook(tb):
