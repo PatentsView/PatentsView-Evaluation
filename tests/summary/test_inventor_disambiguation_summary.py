388ed7382d6c3f34e8375dac4e34dc833bad73ce
Ubuntu <olivier.binette@gmail.com>
2022-06-17 14:24:22 +0000

    refactor tests

--- tests/summary/test_inventor_disambiguation_summary.py
+++
@@ -1,6 +0,0 @@
-from testbook import testbook
-
-
-@testbook("tests/notebooks/summary/inventor.ipynb", execute=True)
-def test_executable_notebook(tb):
-    pass
--- tests/templates/test_inventor_report.py
+++
@@ -1,6 +0,0 @@
-from testbook import testbook
-
-
-@testbook("tests/notebooks/templates/inventor-report.ipynb", execute=True)
-def test_executable_notebook(tb):
-    pass
