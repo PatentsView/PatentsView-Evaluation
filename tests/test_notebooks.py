4255e2f9dac6b458662bacf581541d2ae1820203
OlivierBinette <olivier.binette@gmail.com>
2022-06-21 15:01:25 +0000

    fix tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -2,25 +2,25 @@ from testbook import testbook
 
 
 @testbook("tests/notebooks/templates/inventor-report.ipynb", execute=True)
-def test_executable_notebook(tb):
+def test_inventor_report_notebook(tb):
     pass
 
 
 @testbook("tests/notebooks/metrics/test_pairwise.ipynb", execute=True)
-def test_executable_notebook(tb):
+def test_pairwise_notebook(tb):
     pass
 
 
 @testbook("tests/notebooks/metrics/test_cluster.ipynb", execute=True)
-def test_executable_notebook(tb):
+def test_cluster_notebook(tb):
     pass
 
 
 @testbook("tests/notebooks/benchmark/benchmark-datasets.ipynb", execute=True)
-def test_executable_notebook(tb):
+def test_benchmark_datasets_notebook(tb):
     pass
 
 
 @testbook("tests/notebooks/summary/inventor.ipynb", execute=True)
-def test_executable_notebook(tb):
+def test_summary_inventor_notebook(tb):
     pass
