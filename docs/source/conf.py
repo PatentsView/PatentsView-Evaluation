1001fb8ca0381846c9ac63311a401331ea30a819
Olivier Binette <olivier.binette@gmail.com>
2022-06-27 11:03:45 -0400

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,6 @@ author = "Olivier Binette, Sarvo Madhavan"
 
 # -- General configuration ---------------------------------------------------
 
-html_baseurl = '/build/html/'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
