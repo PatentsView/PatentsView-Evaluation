9ab1b7cd8f66befd01fc06b38ac46aedd2810d84
OlivierBinette <olivier.binette@gmail.com>
2022-07-15 20:13:40 +0000

    fix als dataset; prepare package

--- scripts/als-inventors.py
+++ scripts/als-inventors.py
@@ -3,4 +3,7 @@ try:
 except:
     raise Exception("Unable to import pandas. Please install required dependencies.")
 
-pd.read_csv("data-raw/ALS-dataset/als-inventors.csv").to_csv("pv_evaluation/data/inventor/als-inventors.csv", index=False)
+data = pd.read_csv("data-raw/ALS-dataset/als-inventors.csv")
+data.rename(columns={"inventor-id":"unique-id"}, inplace=True)
+data = data[~data["mention-id"].duplicated(keep='first')]
+data[["mention-id", "unique-id"]].to_csv("pv_evaluation/data/inventor/als-inventors.csv", index=False)
--- scripts/ens-inventors.py
+++ scripts/ens-inventors.py
@@ -4,6 +4,6 @@ except:
     raise Exception("Unable to import pandas. Please install required dependencies.")
 
 data = pd.read_csv("data-raw/patentsview-2015-workshop-datasets/eval/eval_ens.txt", sep="\t", header=None)
-data.columns = ["mention-id", "inventor-id"]
+data.columns = ["mention-id", "unique-id"]
 data["mention-id"] = "US" + data["mention-id"]
 data.to_csv("pv_evaluation/data/inventor/ens-inventors.csv", index=False)
