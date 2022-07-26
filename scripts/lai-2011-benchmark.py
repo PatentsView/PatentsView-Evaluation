4332acb4127de201c4c5affa038573ee9387902f
Olivier Binette <olivier.binette@gmail.com>
2022-07-26 14:34:14 -0400

    Debugging info datasets (#28)
    * Update record sampling analysis to 200 samples
    
    * update datasets to add debugging info
    
    * update changelog

--- scripts/lai-2011-benchmark.py
+++ scripts/lai-2011-benchmark.py
@@ -1,10 +1,26 @@
 try:
     import pandas as pd
+    import wget
+    import zipfile
+    import os
 except:
     raise Exception("Unable to import required dependencies.")
+
+if not os.path.isfile("rawinventor.tsv"):
+    wget.download("https://s3.amazonaws.com/data.patentsview.org/download/rawinventor.tsv.zip")
+    with zipfile.ZipFile("rawinventor.tsv.zip", 'r') as zip_ref:
+        zip_ref.extractall(".")
+    os.remove("rawinventor.tsv.zip")
+
+rawinventor = pd.read_csv("rawinventor.tsv", sep="\t", dtype=str)
 
 lai_benchmark = pd.read_csv("data-raw/Lai-2011-data/benchmark_with_sequence.tsv", sep="\t", dtype=str, header=1)
 
 lai_benchmark["mention-id"] = "US" + lai_benchmark.Patent + "-" + lai_benchmark.Sequence
 lai_benchmark["unique-id"] = lai_benchmark.UniqueID
-lai_benchmark[["mention-id", "unique-id"]].to_csv("pv_evaluation/data/inventor/lai-2011-benchmark.csv", index=False)
+
+rawinventor["mention-id"] = "US" + rawinventor.patent_id + "-" + rawinventor.sequence
+lai_benchmark = lai_benchmark.merge(rawinventor[["mention-id", "name_first", "name_last"]], on="mention-id", how="left")
+
+cols = ["mention-id", "unique-id", "name_first", "name_last"]
+lai_benchmark[cols].to_csv("pv_evaluation/data/inventor/lai-2011-benchmark.csv", index=False)
